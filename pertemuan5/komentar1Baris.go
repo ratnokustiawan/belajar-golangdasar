bb37c9cadcc4bcde25840d2e9ca7459d4b444740
ratnokustiawan <ratnokustiawan@gmail.com>
2023-03-26 14:10:12 +0700

    komentar

---
+++ pertemuan5/komentar1Baris.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Ini adalah komentar single-line
+	fmt.Println("Hello, world!")
+}
---
+++ pertemuan5/komentarBanyakBaris.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	   Ini adalah contoh penggunaan komentar multi-line
+	   Komentar ini dapat mencakup beberapa baris dan sangat berguna
+	   untuk memberikan penjelasan yang lebih detail tentang kode Anda
+	*/
+	fmt.Println("Hello, world!")
+}
